fa65cfb84ef54919bd47241384d52d13957b477a
Yanaranop Naulrahaeng <yanaranop2540mike@hotmail.com>
2021-02-06 05:45:51 +0700

    start

---
+++ main.js
@@ -0,0 +1,114 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e) {
+  var issueDesc = document.getElementById('issueDescInput').value;
+  var issueSeverity = document.getElementById('issueSeverityInput').value;
+  var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+  var issueId = chance.guid();
+  var issueStatus = 'Open';
+  var issuetest = makeid(5);
+  var isDate = Date();
+  var issueDate = isDate.split(' ').slice(1, 5).join(' ');
+  
+
+  var issue = {
+    id: issueId,
+    description: issueDesc,
+    severity: issueSeverity,
+    assignedTo: issueAssignedTo,
+    status: issueStatus,
+    testTo: issuetest,
+    CreateDate: issueDate,
+
+  }
+  // console.log(issue)
+
+  if (localStorage.getItem('issues') == null) {
+    var issues = [];
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  } else {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  }
+
+  document.getElementById('issueInputForm').reset();
+
+  fetchIssues();
+
+  e.preventDefault();
+}
+
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+// console.log(makeid(5));
+
+function setStatusClosed(id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+
+  for (var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues[i].status = 'Closed';
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  fetchIssues();
+}
+
+function deleteIssue(id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+
+  for (var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues.splice(i, 1);
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  fetchIssues();
+}
+
+function fetchIssues() {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  // var issuesListe = document.getElementById('issuesList');
+
+  issuesList.innerHTML = '';
+
+  for (var i = 0; i < issues.length; i++) {
+    var id = issues[i].id;
+    var desc = issues[i].description;
+    var severity = issues[i].severity;
+    var assignedTo = issues[i].assignedTo;
+    var status = issues[i].status;
+    var testTo = issues[i].testTo;
+
+
+
+    var CreateDate = issues[i].CreateDate;
+
+
+
+    issuesList.innerHTML +=   '<div class="well">'+
+                              '<h6>Issue ID: ' + id + '</h6>'+
+                              '<p><span class="label label-info">' + status + '</span></p>'+
+                              '<h3>' + desc + '</h3>'+
+                              '<p><span class="glyphicon glyphicon-time"></span> ' + severity + '  ,' + CreateDate + '</p>'+
+                              '<p><span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
+                              '<p><span class="glyphicon glyphicon-qrcode"></span> ' + testTo + '</p>'+
+                              '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a> '+
+                              '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
+                              '</div>';
+  }
+}
